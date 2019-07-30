7d8b540ad76f332ee72c82febe7de49174f62d7b
Danny Lin <d033016@gmail.com>
2019-07-30 10:46:01 +0800

    add another version

--- reviews.py
+++ reviews.py
@@ -1,13 +1,20 @@
+import random
 data = []
 count = 0
 with open ('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0:
+        if count % 10000 == 0:
             print(len(data))
+r = random.randint(1, 1000000)
+print(data[r])
 sum = 0
 for i in data:
     sum += len(i)
 average = sum / count
 print(average)
+for i in data:
+    if len(i) < 100:
+        print(i)
+        print("----------")
